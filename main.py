7fdcb682adaa0cd71a3efd0a2cf66de706f4984b
maksimbast-ctrl <maksimbast@gmail.com>
2026-07-08 16:06:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "8838803695:AAHaA9DAQwP7RKk-Zy818FajMXVxKKgN5RU"
+
+keyboard = [
+    ["💡 Генерувати ідею"],
+    ["📝 Генерувати назву"],
+    ["🏷️ Хештеги"],
+    ["⭐ PRO"]
+]
+
+reply_markup = ReplyKeyboardMarkup(
+    keyboard,
+    resize_keyboard=True
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Вітаю!\n\n"
+        "Я IdeaBot AI.\n"
+        "Допоможу створювати ідеї для YouTube, TikTok та Instagram.\n\n"
+        "Обери дію нижче 👇",
+        reply_markup=reply_markup
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущений...")
+
+app.run_polling()
